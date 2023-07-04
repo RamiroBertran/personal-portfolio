2148a5c9596766dd70ec4764252a35304cd23941
RamiroBertran <ramirobertra@gmail.com>
2023-07-04 00:33:24 -0300

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+// we can import logo or styles to give to this compoenent
+import "../styles/Box.css";
+import BoxName from "./BoxName";
+
+function App() {
+  return (
+    <div className="Box">
+      <BoxName />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/BoxName.js
@@ -0,0 +1,13 @@
+import "../styles/styles.css";
+
+function BoxName() {
+  return (
+    <div className="BoxName">
+      <div>Ramiro</div>
+      <div>Bertran</div>
+      <div>Full Stack Developer</div>
+    </div>
+  );
+}
+
+export default BoxName;
